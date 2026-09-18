de89b325bddd75bf9d00c518358b4b705a9f12f0
Leouemoto <leonardongr@gmail.com>
2026-09-18 16:06:24 -0300

    exercicios

--- Exercícios/ex8dp.py
+++ Exercícios/ex8dp.py
@@ -4,9 +4,23 @@
 try:
     n = int(input("Digite o número de valores que deseja somar: "))
     soma = 0
-    for i in range(n):
-        numero = float(input(f"Digite o {i+1}º número: "))
+    for contador in range(n):
+        numero = float(input(f"Digite o {contador+1}º número: "))
+        soma = soma + numero
+    print(f"A soma dos {n} números é: {soma}")
+except ValueError:
+    print("O input só pode ser um número.")
+
+try:
+    n = int(input("Digite o número de valores que deseja somar: "))
+    soma = 0
+    contador = 0
+
+    while contador < n:
+        numero = float(input(f"Digite o {contador+1}º número: "))
         soma += numero
+        contador += 1
+
     print(f"A soma dos {n} números é: {soma}")
 except ValueError:
     print("O input só pode ser um número.")
